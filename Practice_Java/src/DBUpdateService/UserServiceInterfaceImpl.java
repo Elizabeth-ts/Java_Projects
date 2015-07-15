f6b5dfa65b6a933de0c2124fc616c88c5f75f757
arvin-tcm <arvin.tcm@gmail.com>
2015-07-15 14:59:46 -0700

    new Projects come2

--- Practice_Java/src/DBUpdateService/UserServiceInterfaceImpl.java
+++ Practice_Java/src/DBUpdateService/UserServiceInterfaceImpl.java
@@ -14,7 +14,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
