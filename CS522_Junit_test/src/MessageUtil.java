7ea8bc3abc6df6f272e7232bf1ba2db981f07061
Arvin Mai <Arvin Mai>
2015-10-23 20:03:05 -0700

    test

---
+++ CS522_Junit_test/src/MessageUtil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Arvin-tcm
+ */
+public class MessageUtil {
+
+    private final String message;
+
+    public MessageUtil(String message) {
+        this.message = message;
+    }
+
+    public String printMessage() {
+        System.out.println(message);
+        return message;
+    }
+
+    public String salutationMessage() {
+        System.out.println(message);
+        return message;
+    }
+}
---
+++ CS522_Junit_test/src/TestJunit2.java
@@ -0,0 +1,25 @@
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Arvin-tcm
+ */
+public class TestJunit2 {
+
+    String message = "Robert";
+    MessageUtil messageUtil = new MessageUtil(message);
+
+    @Test
+    public void testSalutationMessage() {
+        System.out.println("Inside testSalutationMessage()");
+        message = "Hi!" + "Robert";
+        assertEquals(message, messageUtil.salutationMessage());
+    }
+}
